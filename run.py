616a90f82efefdd3434f9bfeaa912cb058b38799
AmarDange <amardange218@gmail.com>
2023-03-18 15:45:37 +0000

    Add spelling correctly

--- run.py
+++ run.py
@@ -72,9 +72,6 @@ while option != 0:
             print(colored(pyfiglet.figlet_format(title), "green"))
         Welcome()
 
-
-        print("Welcome to my computer quiz")
-
         player_name = input("Enter Your Name:  ")
         print("Welcome ", player_name, "to the 'Computer Quiz Game.' ")
 
@@ -93,68 +90,68 @@ while option != 0:
         else:
             print('Incorrect!!!')
 
-        answer = input("What does CPU stand for?")
-        if answer =="central processing unit":
+        answer = input("What does CPU stand for? ")
+        if answer == "central processing unit":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
-        answer = input("What does RAM stand for?")
-        if answer =="random access memory":
+        answer = input("What does RAM stand for? ")
+        if answer == "random access memory":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
-        answer = input("What does HTML stand for?")
-        if answer =="hypertext markup language":
+        answer = input("What does HTML stand for? ")
+        if answer == "hypertext markup language":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
-        answer = input("What does PDF stand for?")
-        if answer =="portable document format":
+        answer = input("What does PDF stand for? ")
+        if answer == "portable document format":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
-        answer = input("What does PNG in computer image format stand for?")
-        if answer =="portable network graphics":
-            print('Correct!!')
-            score += 1
-        else:
-            print('Incorrect!!!')
-
-
-        answer = input("What does GB in computer stand for?")
-        if answer =="giga bytes":
+        answer = input("What does PNG in computer image format stand for? ")
+        if answer == "portable network graphics":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
 
-        answer = input("What does CD in computer stand for?")
-        if answer =="compact disk":
+        answer = input("What does GB in computer stand for? ")
+        if answer == "giga bytes":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
 
-        answer = input("What does DVD stand for?")
-        if answer =="digital video disk":
+        answer = input("What does CD in computer stand for? ")
+        if answer == "compact disk":
             print('Correct!!')
             score += 1
         else:
             print('Incorrect!!!')
 
 
-        answer = input("What does KB stand for?")
-        if answer =="kilo bytes":
+        answer = input("What does DVD stand for? ")
+        if answer == "digital video disk":
+            print('Correct!!')
+            score += 1
+        else:
+            print('Incorrect!!!')
+
+
+        answer = input("What does KB stand for? ")
+        if answer == "kilo bytes":
             print('Correct!!')
             score += 1
         else:
@@ -173,140 +170,3 @@ while option != 0:
 print("Thanks for using the programm. Goodbye")
 
 
-# def option A():
-
-
-# def Welcome():
-#     """
-#     Function to display home page
-#     """
-#     title = "Welcome  to  NUMBER  GUESSING  GAME "
-#     print(colored(pyfiglet.figlet_format(title), "green"))
-# Welcome()
-
-
-# def guess_number():
-#     random_number = random.randint(1, 20)
-#     player_name = input("Enter Your Name:  ")
-#     confirm_play = input("Would you like to start the game? (Enter yes/no): ")
-#     attempts = 0
-
-#     while confirm_play.lower() == "yes":
-
-#         guess = int(input("Guess any number between 1 to 50 : "))
-
-#         if guess < 1 or guess > 50:
-#             print("Please guess any number in the Range.")
-
-#         elif guess == random_number:
-#             print("@@@###$$$Congratulations ", player_name, 'You Win $$$###@@@')
-#             attempts +=1
-#             print("It took you ", attempts, "attempts to win this Game.")
-#             break
-#         elif guess > random_number:
-#             print("Hint: Please try with smaller number.")
-#             attempts +=1
-
-#         elif guess < random_number:
-#             print("Hint: Please try with larger number.")
-#             attempts +=1
-
-#     else:
-#         print("Thanks ", player_name, "for your Time.")
-
-# guess_number()
-
-
-# def option B():
-
-
-# print("Welcome to my computer quiz")
-
-# player_name = input("Enter Your Name:  ")
-# print("Welcome ", player_name, "to the 'Computer Quiz Game.' ")
-
-# playing = input("Do you want to play?")
-
-# if playing !="yes":
-#     quit()
-
-# print("Okay! Let's play! :")
-# score = 0
-
-# answer = input("Is the coding language Python, named after a snake? ")
-# if answer == "no":
-#     print('correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-# answer = input("What does CPU stand for?")
-# if answer =="central processing unit":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-# answer = input("What does RAM stand for?")
-# if answer =="random access memory":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-# answer = input("What does HTML stand for?")
-# if answer =="hypertext markup language":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-# answer = input("What does PDF stand for?")
-# if answer =="portable document format":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-# answer = input("What does PNG in computer image format stand for?")
-# if answer =="portable network graphics":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-
-# answer = input("What does GB in computer stand for?")
-# if answer =="giga bytes":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-
-# answer = input("What does CD in computer stand for?")
-# if answer =="compact disk":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-
-# answer = input("What does DVD stand for?")
-# if answer =="digital video disk":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-
-# answer = input("What does KB stand for?")
-# if answer =="kilo bytes":
-#     print('Correct!!')
-#     score += 1
-# else:
-#     print('Incorrect!!!')
-
-
-# print("You got " + str(score) + " questoins correct!")
-# print("You got " + str((score/10) *100) + "%. ")
