6a4dea6bd2315bc3da1e57703c165bf20a1e615c
AmarDange <amardange218@gmail.com>
2023-03-21 16:06:48 +0000

    update code as per pep8 validator

--- run.py
+++ run.py
@@ -29,30 +29,30 @@ while option != 0:
       
         Welcome("Welcome  to  NUMBER  GUESSING  GAME ")
         def guess_number():
-            random_number = random.randint(1, 50)
+            random_number = random.randint(1, 100)
             print("Welcome ", player_name, "to the 'Number Guessing Game.' ")
             confirm_play = input("Would you like to start the game? (Enter yes/no): ")
             attempts = 0
 
             while confirm_play.lower() == "yes":
 
-                guess = int(input("Guess any number between 1 to 50 : "))
+                guess = int(input("Guess any number between 1 to 100 : "))
 
                 if guess < 1 or guess > 50:
                     print("Please guess any number in the Range.")
 
                 elif guess == random_number:
                     print("@@@ Congratulations ", player_name, 'You Win @@@')
-                    attempts +=1
+                    attempts += 1
                     print("It took you ", attempts, "attempts to win this Game.")
                     break
                 elif guess > random_number:
                     print("Hint: Please try with smaller number.")
-                    attempts +=1
+                    attempts += 1
 
                 elif guess < random_number:
                     print("Hint: Please try with larger number.")
-                    attempts +=1
+                    attempts += 1
 
             else:
                 print("Thanks ", player_name, "for your Time.")
@@ -67,7 +67,6 @@ while option != 0:
         print("Welcome ", player_name, "to the 'Computer Quiz Game.' ")
 
         score = 0
-
         answer = input("What does CSS stand for? ")
         if answer == "cascading style sheets":
             print('correct!!')
@@ -75,7 +74,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> cascading style sheets')
-            
         answer = input("What does CPU stand for? ")
         if answer == "central processing unit":
             print('Correct!!')
@@ -83,7 +81,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> central processing unit')
-
         answer = input("What does RAM stand for? ")
         if answer == "random access memory":
             print('Correct!!')
@@ -91,7 +88,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> random access memory')
-
         answer = input("What does HTML stand for? ")
         if answer == "hypertext markup language":
             print('Correct!!')
@@ -99,7 +95,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> hypertext markup language')
-
         answer = input("What does PDF stand for? ")
         if answer == "portable document format":
             print('Correct!!')
@@ -107,7 +102,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> portable document format')
-
         answer = input("What does PNG in computer image format stand for? ")
         if answer == "portable network graphics":
             print('Correct!!')
@@ -115,8 +109,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> portable network graphics')
-
-
         answer = input("What does GB in computer stand for? ")
         if answer == "gigabyte":
             print('Correct!!')
@@ -124,7 +116,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> gigabyte')
-
         answer = input("What does CD in computer stand for? ")
         if answer == "compact disk":
             print('Correct!!')
@@ -132,8 +123,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> compact disk')
-
-
         answer = input("What does DVD stand for? ")
         if answer == "digital versatile disk":
             print('Correct!!')
@@ -141,8 +130,6 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> digital versatile disk')
-
-
         answer = input("What does JSON stand for? ")
         if answer == "javascript object notation":
             print('Correct!!')
@@ -150,8 +137,7 @@ while option != 0:
         else:
             print('Incorrect!!!')
             print('correct answer is --> javascript object notation')
-
-
+            
         print("You got " + str(score) + " questoins correct!")
         print("You got " + str((score/10) *100) + "%. ")
     else:
